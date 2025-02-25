8e0a99b1dc94c7e61691bc58549dd5041497c8f8
hackerchafa <hmgd.14@gmail.com>
2025-02-25 13:05:52 -0600

    Add files via upload

---
+++ ejercicio5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ejercicio5
+{
+    class Aguinaldo
+    {
+        public double Sueldo { get; set; }
+        
+        public Aguinaldo(double Sueldo)
+        {
+            this.Sueldo = Sueldo;
+        }
+
+        public void MostrarInfo()
+        {
+            Console.WriteLine($"El aguinaldo es de: {CalcularAguinaldo()}");
+        }
+
+        public double CalcularAguinaldo()
+        {
+            double Aguinaldo = Sueldo*15;
+            return Aguinaldo;
+        }
+    }
+
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cual es tu sueldo?");
+            double Sueldo = Convert.ToDouble(Console.ReadLine());
+
+            Aguinaldo miAguinaldo = new Aguinaldo(Sueldo);
+
+            miAguinaldo.MostrarInfo();
+        }
+    }
+}
